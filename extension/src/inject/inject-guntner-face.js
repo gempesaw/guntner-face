b59b1830918c8d10ad7bd62e5ce058a97555e534
Daniel Gempesaw <gempesaw@gmail.com>
2016-02-23 18:46:34 -0500

    Load image from extension instead of s3

--- extension/src/inject/inject-guntner-face.js
+++ extension/src/inject/inject-guntner-face.js
@@ -1,6 +1,6 @@
 var images = document.getElementsByTagName('img');
 Array.prototype.slice.call(images).forEach(function (img) {
     if (Math.random() > 0.5 ) {
-        img.src = "http://s.qa.origin.sharecare.com/honeydew/2016-02-23.jpg";
+        img.src = chrome.extension.getURL("images/guntner.jpg");
     }
 });
