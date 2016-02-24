8fc10397d4763e6d75f747315f383f0dac208259
Daniel Gempesaw <gempesaw@gmail.com>
2016-02-23 23:17:56 -0500

    Choose your own ~~adventure~~ guntner

--- extension/src/inject/inject-guntner-face.js
+++ extension/src/inject/inject-guntner-face.js
@@ -4,9 +4,18 @@ function faceGuntner () {
     var images = document.getElementsByTagName('img');
     Array.prototype.slice.call(images).forEach(function (img) {
         if (Math.random() > 0.5 ) {
-            img.src = chrome.extension.getURL("images/guntner.jpg");
+            img.src = chrome.extension.getURL(chooseGuntner());
         }
     });
+}
+
+function chooseGuntner () {
+    var guntnerImages = [
+        'images/guntner.jpg'
+    ];
+
+    var index = Math.floor(Math.random() * guntnerImages.length);
+    return guntnerImages[index];
 }
 
 faceGuntner();
