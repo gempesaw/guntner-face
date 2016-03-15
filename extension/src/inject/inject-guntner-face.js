24a54b596719503060c57b076e585a019c495053
Daniel Gempesaw <gempesaw@gmail.com>
2016-03-15 19:11:08 -0400

    Face a huge guntner

--- extension/src/inject/inject-guntner-face.js
+++ extension/src/inject/inject-guntner-face.js
@@ -5,7 +5,8 @@ function chooseGuntner () {
         'images/guntner1.jpg',
         'images/guntner2.png',
         'images/guntner3.png',
-        'images/guntner4.png'
+        'images/guntner4.png',
+        'images/guntner5.png'
     ];
 
     var index = Math.floor(Math.random() * guntnerImages.length);
@@ -21,4 +22,12 @@ function faceGuntner () {
     });
 }
 
-faceGuntner();
+function guntnerIsReady(fn) {
+    if (document.readyState != 'loading'){
+        faceGuntner();
+    } else {
+        document.addEventListener('DOMContentLoaded', faceGuntner);
+    }
+}
+
+guntnerIsReady();
